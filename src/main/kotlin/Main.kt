776e557f762a25887b1447c76d10c4b570be05de
DJNooZ <loktevmaksim@yandex.ru>
2025-12-16 22:58:52 +0300

    KTB-04 parsing dictionary file and printing to console

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,9 +7,26 @@ fun main() {
 
     if (!wordsFile.exists()) { return }
 
-    val strings = wordsFile.readLines()
+    val lines = wordsFile.readLines()
 
-    strings.forEach {
-        println(it)
+    val dictionary = mutableListOf<Word>()
+
+    lines.forEach { line ->
+        val newLine = line.split("|")
+
+        val model = Word(
+            text = newLine[0],
+            translate = newLine[1],
+            correctAnswersCount = newLine[2].toIntOrNull() ?: 0
+        )
+        dictionary.add(model)
     }
+
+    println(dictionary)
 }
+
+data class Word(
+    val text: String,
+    val translate: String,
+    val correctAnswersCount: Int = 0,
+)
