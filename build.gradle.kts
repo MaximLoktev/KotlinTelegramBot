9640c90b3913877554667fb800b69d22f4ad0f0b
DJNooZ <loktevmaksim@yandex.ru>
2026-01-29 23:23:08 +0300

    KTB-23 posted bot on server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+
 plugins {
+    application
     kotlin("jvm") version "2.3.0"
     kotlin("plugin.serialization") version "2.3.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "org.example"
@@ -11,6 +14,7 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 }
@@ -20,4 +24,19 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(24)
+}
+
+application {
+    mainClass.set("org.example.TelegramKt")
+}
+
+tasks.shadowJar {
+    manifest {
+        attributes(
+            "Main-Class" to "org.example.TelegramKt"
+        )
+    }
+    // Убираем суффиксы из имени файла
+    archiveClassifier.set("")
+    archiveVersion.set("")
 }
